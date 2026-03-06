93b725c98f118ac442070082e4c2216ee6816c61
Jon Witherspoon <119535757+theknoxtech@users.noreply.github.com>
2026-03-05 22:36:29 -0500

    Refactor App and UI components; integrate TransactionWindow, enhance Sidebar functionality, and improve layout configuration

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from models import Budget, Transaction, Category, Payee
 from database import get_categories, add_transaction
 from datetime import datetime
 from utils import validate_input
-from ui import Sidebar, Toolbar, MainView
+from ui import Sidebar, Toolbar, MainView, TransactionWindow
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -11,100 +11,34 @@ class App(customtkinter.CTk):
         self.geometry("800x600")
         self.title("Budgetry")
         self.grid_columnconfigure(0, weight=0)
-        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)
+        
+        # Sidebar
+        self.sidebar = Sidebar(master=self, open_transaction_window=self.open_transaction_window)
+        self.sidebar.grid(row=1,column=0,sticky="nsw")
+        
+        #Main View
+        self.mainview = MainView(master=self)
+        self.mainview.grid(row=1, column=1, sticky="nsew")
         
         # Toolbar
         self.toolbar = Toolbar(master=self)
-        self.toolbar.grid(row=0, column=0, sticky="ew", columnspan=2)
+        self.toolbar.grid(row=0, column=0, columnspan=2, sticky="ew")
         
-        # Sidebar
-        self.sidebar = Sidebar(master=self)
-        self.sidebar.grid(row=1,column=0,sticky="nsw")
-        
-        self.mainview = MainView(master=self)
-        self.mainview.grid(row=1, column=0, sticky="ew", columnspan=2)
-        
-        self.budget = Budget(name="Main Budget")
-
-        # TODO Create button class in UI.py
-        self.add_transaction_btn = customtkinter.CTkButton(self, text="Add Transaction", command=self.open_transaction_window)
-        #self.add_transaction_btn.pack(pady=20)
+        #self.budget = Budget(name="Main Budget")
         
     def open_transaction_window(self):
         categories = get_categories()
         category_map = {category.name: category.id for category in categories}
         self.transaction_window = TransactionWindow(self, self.handle_save_success, category_map)
+        
     
     def handle_save_success(self, transaction):
         add_transaction(transaction)
         # TODO this needs to be a notifcation
         print("Transaction has been saved!")
-        
-
-class TransactionWindow(customtkinter.CTkToplevel):
-    def __init__(self, master, on_save_callback, category_map):
-        super().__init__(master)
-        self.title("Add New Transaction")
-        self.geometry("300x400")
-        
-        self.on_save_callback = on_save_callback
-        self.category_map = category_map
-        
-        # TODO Change to date selector
-        
-        self.date = customtkinter.CTkEntry(self,placeholder_text="YYYY-MM-DD")
-        self.date.pack(pady=10)
-        today = datetime.now().strftime("%Y-%m-%d")
-        self.date.insert(0,today)
-        
-        
-        # TODO Change to dropdown
-        self.payee = customtkinter.CTkEntry(self, placeholder_text="Payee")
-        self.payee.pack(pady=10)
-        
-        # TODO Change to dropdown
-        categories = get_categories()
-        self.category_map = {category.name: category.id for category in categories}
-        self.category_dropdown = customtkinter.CTkOptionMenu(self, values=list(self.category_map.keys()))
-        self.category_dropdown.pack(pady=10)
-        
-        self.memo = customtkinter.CTkEntry(self, placeholder_text="Memo")
-        self.memo.pack(pady=10)
-        
-        self.amount = customtkinter.CTkEntry(self, placeholder_text="Amount")
-        self.amount.pack(pady=10)
-        
-        self.save_button = customtkinter.CTkButton(self, text="Save Transaction", command=self.handle_save)
-        self.save_button.pack(pady=20)
-        
-    def handle_save(self):
-        
-        date = self.date.get()
-        payee = self.payee.get()
-        
-        selected_category = self.category_dropdown.get()
-        category_id = self.category_map.get(selected_category)
-        
-        memo = self.memo.get()
-        amount_input = self.amount.get()
-        if not validate_input(amount_input):
-
-            # TODO this needs to be a notification
-            print("Invalid entry format: Please use 0.00!")
-            return
-        
-        try:
-            clean_amount = amount_input.replace(',', '')
-            new_transaction = Transaction(date=date, payee=payee, amount=clean_amount, memo=memo, category_id=category_id)
-            self.on_save_callback(new_transaction)
-            self.destroy()
-        except ValueError:
-            print("There was a problem with the amount conversion")
-            
-        
-        
-        
 
 
 
--- app/ui.py
+++ app/ui.py
@@ -1,9 +1,12 @@
 import customtkinter
-import utils
+from utils import validate_input
+from database import get_transaction, get_categories, get_payees
+from models import Transaction, Category, Payee
+from datetime import datetime
 
 
 class Sidebar(customtkinter.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, open_transaction_window):
         super().__init__(master)
         
         #self.grid_columnconfigure(0, weight=0)
@@ -14,11 +17,15 @@ class Sidebar(customtkinter.CTkFrame):
         #self.grid_rowconfigure(5, weight=0)
         #self.grid_rowconfigure(6, weight=0)
         
-        view_transaction_btn  = customtkinter.CTkButton(self, text="View Transactions")
-        view_transaction_btn.grid(row=1, column=0, padx=10, pady=10)
         
-        category_btn = customtkinter.CTkButton(self, text="Categories")
-        category_btn.grid(row=2, column=0, padx=10, pady=10)
+        self.add_transaction_btn = customtkinter.CTkButton(self, text="Add Transaction", command=open_transaction_window)
+        self.add_transaction_btn.grid(row=1, column=0, padx=10, pady=10)
+        
+        self.view_transaction_btn  = customtkinter.CTkButton(self, text="View Transactions")
+        self.view_transaction_btn.grid(row=2, column=0, padx=10, pady=10)
+        
+        self.category_btn = customtkinter.CTkButton(self, text="Categories")
+        self.category_btn.grid(row=3, column=0, padx=10, pady=10)
 
 class Toolbar(customtkinter.CTkFrame):
     def __init__(self,master):
@@ -51,4 +58,65 @@ class MainView(customtkinter.CTkFrame):
         
         mainView_btn_1 = customtkinter.CTkButton(self, text="Main View Placeholder")
         mainView_btn_1.grid(padx=10, pady=10)
+
+class TransactionWindow(customtkinter.CTkToplevel):
+    def __init__(self, master, on_save_callback, category_map):
+        super().__init__(master)
+        self.title("Add New Transaction")
+        self.geometry("300x400")
+        
+        self.on_save_callback = on_save_callback
+        self.category_map = category_map
+        
+        # TODO Change to date selector
+        
+        self.date = customtkinter.CTkEntry(self,placeholder_text="YYYY-MM-DD")
+        self.date.pack(pady=10)
+        today = datetime.now().strftime("%Y-%m-%d")
+        self.date.insert(0,today)
+        
+        
+        # TODO Change to dropdown
+        self.payee = customtkinter.CTkEntry(self, placeholder_text="Payee")
+        self.payee.pack(pady=10)
+        
+        # TODO Change to dropdown
+        categories = get_categories()
+        self.category_map = {category.name: category.id for category in categories}
+        self.category_dropdown = customtkinter.CTkOptionMenu(self, values=list(self.category_map.keys()))
+        self.category_dropdown.pack(pady=10)
+        
+        self.memo = customtkinter.CTkEntry(self, placeholder_text="Memo")
+        self.memo.pack(pady=10)
+        
+        self.amount = customtkinter.CTkEntry(self, placeholder_text="Amount")
+        self.amount.pack(pady=10)
+        
+        self.save_button = customtkinter.CTkButton(self, text="Save Transaction", command=self.handle_save)
+        self.save_button.pack(pady=20)
+        
+    def handle_save(self):
+        
+        date = self.date.get()
+        payee = self.payee.get()
+        
+        selected_category = self.category_dropdown.get()
+        category_id = self.category_map.get(selected_category)
+        
+        memo = self.memo.get()
+        amount_input = self.amount.get()
+        if not validate_input(amount_input):
+
+            # TODO this needs to be a notification
+            print("Invalid entry format: Please use 0.00!")
+            return
+        
+        try:
+            clean_amount = amount_input.replace(',', '')
+            new_transaction = Transaction(date=date, payee=payee, amount=clean_amount, memo=memo, category_id=category_id)
+            self.on_save_callback(new_transaction)
+            self.destroy()
+        except ValueError:
+            print("There was a problem with the amount conversion")
+            
         
